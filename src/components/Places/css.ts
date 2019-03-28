58c2c5826a3c05d0c32cdee104399618bb004cb9
mpoapostolis <mpoapostolis@gmail.com>
2019-03-29 00:10:59 +0200

    Places layout: overflow scroll

--- src/components/Places/css.ts
+++ src/components/Places/css.ts
@@ -1,5 +1,13 @@
 import { css } from "emotion"
+import { mq } from "../../css"
 
 export const container = css`
   padding: 10px;
+  overflow: scroll;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  ${mq[0]} {
+    justify-content: center;
+  }
 `
