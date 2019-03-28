16e746ed2a0f066b87345d4b83ddcde1a4ae51c7
mpoapostolis <mpoapostolis@gmail.com>
2019-03-29 00:09:34 +0200

    WIP: Card

--- src/components/Card/css.ts
+++ src/components/Card/css.ts
@@ -5,6 +5,7 @@ export const container = css`
   height: 382px;
   border-radius: 4px;
   box-shadow: 1px 1px 2px 1px #0003;
+  margin: 10px 0 10px 0;
 `
 export const imgCont = css`
   width: 350px;
@@ -12,6 +13,8 @@ export const imgCont = css`
   overflow: hidden;
   cursor: pointer;
   img {
+    width: 350px;
+    height: 200px;
     object-fit: cover;
     &:hover {
       transform: scale3d(1.3, 1.3, 1.3);
@@ -21,5 +24,39 @@ export const imgCont = css`
 `
 
 export const infoCont = css`
-  padding: 0 15px;
+  height: calc(100% - 220px);
+  display: flex;
+  justify-content: space-between;
+  flex-direction: column;
+  padding: 10px 15px 10px 15px;
+`
+export const body = css`
+  height: calc(100% - 40px);
+`
+
+export const footer = css`
+  height: 40px;
+  align-items: center;
+  display: flex;
+`
+
+export const favorite = css`
+  cursor: pointer;
+  fill: #f002;
+  &.favorited {
+    fill: #f00;
+  }
+  &:hover {
+    fill: #f00;
+  }
+  transition: 0.25s;
+`
+
+export const location = css`
+  cursor: pointer;
+  fill: #5aabfaca;
+  &:hover {
+    fill: #5aabfa;
+  }
+  transition: 0.25s;
 `
--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,35 @@
 import React from "react"
-import { container, imgCont, infoCont } from "./css"
+import { container, imgCont, infoCont, footer, body } from "./css"
+import I18n from "../../I18n"
+import Button from "../Button"
+import { ReactComponent as Favorite } from "./favorite.svg"
+import { IPlaceDetail } from "../../redux/reducers/places"
 
-interface IProps {}
-function Card() {
+interface IProps extends IPlaceDetail {}
+function Card(props: IProps) {
   return (
-    <div className={container}>
-      <div className={imgCont}>
-        <img
-          src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
-          alt=":)"
-        />
-      </div>
-      <div className={infoCont}>
-        <section>
-          <h1>Header</h1>
-          <article>
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-            bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla bla
-          </article>
-        </section>
-      </div>
-    </div>
+    <I18n.Consumer>
+      {t => (
+        <div className={container}>
+          <div className={imgCont}>
+            <img
+              src="https://material-components.github.io/material-components-web-catalog/static/media/photos/3x2/2.jpg"
+              alt=":)"
+            />
+          </div>
+          <div className={infoCont}>
+            <section className={body}>
+              <h1>Header</h1>
+              <article />
+            </section>
+            <section className={footer}>
+              <Button>{t("int.info")}</Button>
+              <Favorite />
+            </section>
+          </div>
+        </div>
+      )}
+    </I18n.Consumer>
   )
 }
 
